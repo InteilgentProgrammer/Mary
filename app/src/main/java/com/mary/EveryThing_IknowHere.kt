0b5a91f419f848ac01fb108383d1959d0177162b
aaa <aaa@aaa.com>
2022-01-24 18:19:28 +0200

    1

--- app/src/main/java/com/mary/EveryThing_IknowHere.kt
+++ app/src/main/java/com/mary/EveryThing_IknowHere.kt
@@ -1,2 +1,12 @@
 package com.mary
+// Hi this is the file i put into every thing i learn
 
+//the commands in Window power shell
+/*
+* cd / : to out from directory ex: PS C:\Users\MAHMOUD> cd /
+* mkdir : to make directory ex: PS C:\Users\MAHMOUD\Downloads> mkdir myGitHub
+* cd : to move from location to either location ex: PS C:\> cd Users\MAHMOUD\Downloads\
+* dir : to Explorer file in directory ex: PS C:\Users\MAHMOUD\Downloads> dir
+* git clone : to Download from github or others control version system ex: PS C:\Users\MAHMOUD\Downloads> git clone https://github.com/InteilgentProgrammer/GitRepo.git
+*
+* */
