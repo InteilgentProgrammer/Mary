61b8a2cac80cf02f42d90ae491afcdb791bf3cfe
aaa <aaa@aaa.com>
2022-01-24 17:48:01 +0200

    1

--- app/src/main/java/com/mary/MainActivity.kt
+++ app/src/main/java/com/mary/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +12,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
