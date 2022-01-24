c24799781ff31867e8d179a0ac12bbff8921fd1e
aaa <aaa@aaa.com>
2022-01-24 22:56:57 +0200

    add some line in file EveryThing_IKnowHere.kt

--- app/src/main/java/com/mary/EveryThing_IknowHere.kt
+++ app/src/main/java/com/mary/EveryThing_IknowHere.kt
@@ -7,6 +7,8 @@ package com.mary
 * cd : to move from location to either location ex: PS C:\> cd Users\MAHMOUD\Downloads\
 * dir : to Explorer file in directory ex: PS C:\Users\MAHMOUD\Downloads> dir
 * git clone : to Download from github or others control version system ex: PS C:\Users\MAHMOUD\Downloads> git clone https://github.com/InteilgentProgrammer/GitRepo.git
-*
+* git status : to Check if the file and directory are adding or not ex:PS C:\Users\MAHMOUD\Downloads\GitRepo> git status
+* git add : to adding the file and directory ex: PS C:\Users\MAHMOUD\Downloads\GitRepo\New folder> git add *
+* git commit m-"the message here any" : to save the change other wise to back to commit when something is going wrong ex:
 * */
 // End
